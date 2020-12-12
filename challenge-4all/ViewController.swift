6115ec5402192bfc28c1e72a821c9abda2bc04c0
Vinicius Silva Alencar <vinicius.s.alencar@outlook.com>
2020-12-12 11:38:48 -0400

    Initial Commit

---
+++ challenge-4all/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  challenge-4all
+//
+//  Created by Vinicius Alencar on 12/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
